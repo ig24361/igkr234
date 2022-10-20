58d6f2125c03655faa5bcc9b9c877295136666cd
ig24361 <igorkremic123@gmail.com>
2022-10-20 09:33:17 +0200

    update
    update

---
+++ Sabiranje 2 broja/Sabiranje 2 broja/appdata/JavaScript1.js
@@ -0,0 +1,5 @@
+﻿function fun() {
+    var x = parseInt(document.getElementsByClassName("tekst")[0].value);
+    var y = parseInt(document.getElementsByClassName("tekst")[1].value);
+    document.getElementsByTagName("input")[3].value = x + y;
+}
