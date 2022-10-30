02d4ab4de5e6bda79ff4d412c5ceb930c8b4687b
ig24361 <113453901+ig24361@users.noreply.github.com>
2022-10-30 11:00:25 +0100

    Delete Sabiranje 2 broja directory

--- Sabiranje 2 broja/Sabiranje 2 broja/appdata/JavaScript1.js
+++
@@ -1,5 +0,0 @@
-﻿function fun() {
-    var x = parseInt(document.getElementsByClassName("tekst")[0].value);
-    var y = parseInt(document.getElementsByClassName("tekst")[1].value);
-    document.getElementsByTagName("input")[3].value = x + y;
-}
